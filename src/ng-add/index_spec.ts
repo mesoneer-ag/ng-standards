2a7b03fe6749dd91836aaa62ac918db4a60435f7
Dominik Forster <domi@imod.ch>
2020-10-22 16:42:04 +0200

    test: :white_check_mark: add unit tests for prettier and stylelint schematics

--- src/ng-add/index_spec.ts
+++ src/ng-add/index_spec.ts
@@ -1,16 +1,39 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import {
+  SchematicTestRunner,
+  UnitTestTree
+} from '@angular-devkit/schematics/testing';
+import { Schema } from '@schematics/angular/workspace/schema';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
+const workspaceOptions: Schema = {
+  name: 'workspace',
+  newProjectRoot: 'projects',
+  version: '10.0.0'
+};
+
 describe('ng-add', () => {
+  const runner = new SchematicTestRunner('schematics', collectionPath);
+  let appTree: UnitTestTree;
+
+  beforeEach(async () => {
+    appTree = await runner
+      .runExternalSchematicAsync(
+        '@schematics/angular',
+        'workspace',
+        workspaceOptions
+      )
+      .toPromise();
+  });
+
   it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
     const tree = await runner
-      .runSchematicAsync('ng-add', {}, Tree.empty())
+      .runSchematicAsync('ng-add', {}, appTree)
       .toPromise();
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files.includes('/.stylelintrc')).toBeTruthy();
+    expect(tree.files.includes('/.prettierrc')).toBeTruthy();
+    expect(tree.files.includes('/.prettierignore')).toBeTruthy();
   });
 });
--- src/prettier/index_spec.ts
+++ src/prettier/index_spec.ts
@@ -1,16 +1,47 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import {
+  SchematicTestRunner,
+  UnitTestTree
+} from '@angular-devkit/schematics/testing';
+import { Schema } from '@schematics/angular/workspace/schema';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
+const workspaceOptions: Schema = {
+  name: 'workspace',
+  newProjectRoot: 'projects',
+  version: '10.0.0'
+};
+
 describe('prettier', () => {
-  it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
+  const runner = new SchematicTestRunner('schematics', collectionPath);
+  let appTree: UnitTestTree;
+
+  beforeEach(async () => {
+    appTree = await runner
+      .runExternalSchematicAsync(
+        '@schematics/angular',
+        'workspace',
+        workspaceOptions
+      )
+      .toPromise();
+  });
+
+  it('should create prettier files', async () => {
     const tree = await runner
-      .runSchematicAsync('prettier', {}, Tree.empty())
+      .runSchematicAsync('prettier', {}, appTree)
       .toPromise();
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files.includes('/.prettierrc')).toBeTruthy();
+    expect(tree.files.includes('/.prettierignore')).toBeTruthy();
+  });
+
+  it('should add lint rule', async () => {
+    const tree = await runner
+      .runSchematicAsync('prettier', {}, appTree)
+      .toPromise();
+
+    const content = tree.readContent('/package.json');
+    expect(content).toMatch('prettier -c *');
   });
 });
